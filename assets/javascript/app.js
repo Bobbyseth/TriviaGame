2585854e9ebfe1a977aef902b2d52dee8de2cfb8
Bobbyseth <seth.griggs007@gmail.com>
2018-01-28 17:17:44 -0600

    Timer is complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,20 @@
-//ready out document here:
-$(document).ready(function(){
-//timer shit...
-var answer = document.getElementById("q1");
-alert(form.element["q1"].value);
-
-
-
-
-
-
-var count=60;
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-function timer() {
-  count=count-1;
-  document.querySelector("#time").innerHTML = counter;
-  if (count <= 0) {
-     clearInterval(counter);
-
-     //
-     return;
-  }
-
+// timer variable
+var timeLeft = 60;
+// function for running the timer
+function run() {
+   clock = setInterval(decrement, 1000);
 }
-
-
-
-
-
-var correct = 0;
-var wrong = 0;
-//psuedo code for checking correct answer
-// every question (q1-q10) has a set of answers (q1a1-q1a3). Each question will have a correct answer set, so for q1, q1a2 might be the correct answer. At then end of the timer, the js will evalute each question/amswer combo to see which is right or wrong, then show how many of each the user recieved.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+run();
+//funcion for the timer
+function decrement() {
+    timeLeft--;
+    $("#time").html("<h3>Time Left: " + timeLeft + "</h3>");
+    if (timeLeft === 0) {
+        outOfTime();
+    }
+}
+//function to run when time runs out
+function outOfTime() {
+  clearInterval(clock);
+  $("#time").html("<h3>You're out of time</h3>");
 }
