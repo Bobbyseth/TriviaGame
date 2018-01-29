a14ab1d70083083a48bb685898970a327ab056a3
Bobbyseth <seth.griggs007@gmail.com>
2018-01-28 20:54:44 -0600

    some more changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,40 @@
+// vars defined here:
 var correct = 0;
 var wrong = 0;
-// timer variable
-var timeLeft = 5;
+var timeLeft = 60;
 // function for running the timer
 function run() {
    clock = setInterval(decrement, 1000);
-
 }
 run();
+
 //funcion for the timer
 function decrement() {
     timeLeft--;
     $("#time").html("<h3>Time Left: " + timeLeft + "</h3>");
     if (timeLeft === 0) {
         outOfTime();
+        results();
     }
 }
+
 //function to run when time runs out
 function outOfTime() {
   clearInterval(clock);
   $("#time").html("<h3>You're out of time</h3>");
   document.getElementById("quiz").submit();
-  check();
+  document.getElementById("quiz").reset();
 }
 
-function check() {
-  $('#quiz input').on('change', function() {
-    var a1 = ($('input[name=q1]:checked', '#quiz').val());
-      if (a1 == 1) {
-        correct++;
+//function for checking results, not working...
+function results() {
+  if (document.getElementById("q1").checked) {
+    var answer1 = document.getElementsByName("q1").value;
+    if (answer1 == 1) {
+      correct++;
       }
-      else {
+    if (answer1 != 1) {
       wrong++;
       }
-  });
-  $('#quiz input').on('change', function() {
-    var a2 = ($('input[name=q2]:checked', '#quiz').val());
-      if (a2 == 1) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a3 = ($('input[name=q3]:checked', '#quiz').val());
-      if (a3 == 1) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a4 = ($('input[name=q4]:checked', '#quiz').val());
-      if (a4 == 1) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a5 = ($('input[name=q5]:checked', '#quiz').val());
-      if (a1 == 5) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a6 = ($('input[name=q6]:checked', '#quiz').val());
-      if (a1 == 6) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a7 = ($('input[name=q7]:checked', '#quiz').val());
-      if (a1 == 7) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a8 = ($('input[name=q8]:checked', '#quiz').val());
-      if (a1 == 8) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a9 = ($('input[name=q9]:checked', '#quiz').val());
-      if (a1 == 9) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
-  $('#quiz input').on('change', function() {
-    var a10 = ($('input[name=q10]:checked', '#quiz').val());
-      if (a10 == 1) {
-        correct++;
-      }
-      else {
-      wrong++;
-      }
-  });
+  }
 }
